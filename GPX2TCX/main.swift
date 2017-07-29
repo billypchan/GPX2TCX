fa9d1668ca70d736257425b288418bc9c52e40aa
Bill Chan MBA <billypchan@gmail.com>
2017-07-30 00:37:58 +0200

    Initial Commit

---
+++ GPX2TCX/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GPX2TCX
+//
+//  Created by chan bill on 30/7/2017.
+//  Copyright © 2017 chan bill. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
