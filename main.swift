6e38a01c8273f9582de4064ce70080222351a7a8
Bill Chan MBA <billypchan@gmail.com>
2017-07-30 16:10:22 +0200

    copy() of AEXMLELement distance calculation

--- main.swift
+++ main.swift
@@ -8,10 +8,33 @@
 
 import Foundation
 import AEXML
+import CoreLocation
+
+extension AEXMLElement {
+    func copy() -> AEXMLElement {
+        let clone = AEXMLElement(name: self.name, attributes: self.attributes)
+        
+        for child in self.children {
+            clone.addChild(child.copy())
+        }
+        
+        return clone
+    }
+}
+
+func distanceFormCoordinates(lat0: Double, log0: Double, lat1: Double, log1: Double) -> Double{
+    let coordinate0 = CLLocation(latitude: lat0, longitude: log0)
+    let coordinate1 = CLLocation(latitude: lat1, longitude: log1)
+    
+    let distanceInMeters = coordinate0.distance(from: coordinate1)
+    
+    return distanceInMeters
+}
+
 
 func readXML(_ fullPath: String) -> AEXMLDocument?{
     var xmlToParse = "?"
-
+    
     //reading
     if let url = URL(string:fullPath) {
         do {
@@ -25,7 +48,7 @@ func readXML(_ fullPath: String) -> AEXMLDocument?{
     
     
     do {
-//        print("xmlToParse")
+        //        print("xmlToParse")
         let xmlDoc = try AEXMLDocument(xml: xmlToParse)
         return xmlDoc
     }
@@ -42,13 +65,12 @@ func readXML(_ fullPath: String) -> AEXMLDocument?{
 let fullPath = "file:///Users/chanbill/Desktop/GPX2TCX/2017-07-28_20218653_birkholzer-chaussee-blumberg-birkholz-loop-from-senefelderplatz_export.gpx"
 
 let fullPathTCXTemplate = "file:///Users/chanbill/Desktop/GPX2TCX/template.tcx"
+let fullPathTCXOutput = "file:///Users/chanbill/Desktop/GPX2TCX/output.tcx"
 
 let xmlGPX = readXML(fullPath)
 let xmlTCXtemplate = readXML(fullPathTCXTemplate)
 
 var foldersTemplate = xmlTCXtemplate?.root["Folders"]
-//let val = folders?["Courses"]["CourseFolder"]["CourseNameRef"]["Id"].last?.value
-//let val = folders?.xmlCompact
 
 foldersTemplate?["Courses"]["CourseFolder"]["CourseNameRef"]["Id"].value = nil
 
@@ -62,8 +84,6 @@ trackpointTemplate?["Position"]["LatitudeDegrees"].value = "0"
 trackpointTemplate?["Position"]["LongitudeDegrees"].value = "0"
 trackpointTemplate?["AltitudeMeters"].value = "0"
 trackpointTemplate?["DistanceMeters"].value = "0"
-///FIXME: calc DistanceMeters
-trackpointTemplate?["DistanceMeters"].removeFromParent()
 
 
 //print(trackpointTemplate?.xml)
@@ -96,6 +116,10 @@ let trkName = xmlGPX?.root["trk"]["name"].value
 folders["Courses"]["CourseFolder"]["CourseNameRef"]["Id"].value = trkName
 
 let course = trainingCenterDatabase.addChild(name:"Courses").addChild(name:"Course")
+
+let name = course.addChild(name:"Name")
+name.value = trkName
+
 let lap = course.addChild(lapTemplate!)
 
 let beginPosition = xmlGPX?.root["trk"]["trkseg"]["trkpt"].first?.attributes
@@ -106,18 +130,39 @@ lap["BeginPosition"]["LongitudeDegrees"].value = beginPosition?["lon"]
 lap["EndPosition"]["LatitudeDegrees"].value = endPosition?["lat"]
 lap["EndPosition"]["LongitudeDegrees"].value = endPosition?["lon"]
 
+var lastLat = Double(0)
+var lastLog = Double(0)
+var totalDistance = Double(-1)
 
 for trkpt in (xmlGPX?.root["trk"]["trkseg"]["trkpt"].all)! {
-    let track = course.addChild(name:"Track").addChild(trackpointTemplate!)
-
+    let track = course.addChild(name:"Track").addChild((trackpointTemplate?.copy())!)
+    
     track["Time"].value = trkpt["time"].value
+    let lat = Double(trkpt.attributes["lat"]!)!
+    let log = Double(trkpt.attributes["lon"]!)!
+    
     track["Position"]["LatitudeDegrees"].value = trkpt.attributes["lat"]
     track["Position"]["LongitudeDegrees"].value = trkpt.attributes["lon"]
     track["AltitudeMeters"].value = trkpt["ele"].value
-//    track["DistanceMeters"].value = "0"
-
-//    break
+    if totalDistance == -1 {
+        track["DistanceMeters"].value = "0"
+        totalDistance = 0
+    }
+    else {
+        let distance = distanceFormCoordinates(lat0: lastLat, log0: lastLog, lat1: lat, log1: log)
+        totalDistance += distance
+        track["DistanceMeters"].value = String(totalDistance)
+    }
+    
+    lastLat = lat
+    lastLog = log
 }
 
 // prints the same XML structure as original
-print(xmlTCXoutput.xml)
+//print(xmlTCXoutput.xml)
+
+//writing
+do {
+    try xmlTCXoutput.xml.write(to: URL(string:fullPathTCXOutput)!, atomically: false, encoding: String.Encoding.utf8)
+}
+catch {/* error handling here */}
